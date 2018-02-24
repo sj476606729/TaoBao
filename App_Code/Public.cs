b3ba6396d9f7fc9cf56058b9ff547801d8f1cb33
sj476606729 <476606729@qq.com>
2018-02-24 11:57:12 +0800

    服务器路径修改

--- App_Code/Public.cs
+++ App_Code/Public.cs
@@ -13,10 +13,10 @@ namespace Room
     {
         public static List<string> List_Data = new List<string>();
         public static DataTable Data_Table = new DataTable();
-        //public static string DataBaseUrl = "server=QT-20180210RVOF\\SQLEXPRESS;user id=sa;pwd=sj76606729;database=taobao;";
-        //public static string DataBaseUrl_ = "server=QT-20180210RVOF\\SQLEXPRESS;user id=sa;pwd=sj76606729;database=Product_db;";
-        public static string DataBaseUrl = "server=LAPTOP-03G3SDKH\\SQLEXPRESS;user id=sa;pwd=sj76606729;database=taobao;";
-        public static string DataBaseUrl_ = "server=LAPTOP-03G3SDKH\\SQLEXPRESS;user id=sa;pwd=sj76606729;database=Product_db;";
+        public static string DataBaseUrl = "server=QT-20180210RVOF\\SQLEXPRESS;user id=sa;pwd=sj76606729;database=taobao;";
+        public static string DataBaseUrl_ = "server=QT-20180210RVOF\\SQLEXPRESS;user id=sa;pwd=sj76606729;database=Product_db;";
+        //public static string DataBaseUrl = "server=LAPTOP-03G3SDKH\\SQLEXPRESS;user id=sa;pwd=sj76606729;database=taobao;";
+        //public static string DataBaseUrl_ = "server=LAPTOP-03G3SDKH\\SQLEXPRESS;user id=sa;pwd=sj76606729;database=Product_db;";
         public Public()
         {
             //
